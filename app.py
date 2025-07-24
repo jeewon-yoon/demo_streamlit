fe17d3a40fe4d0ee6fbb2183770f653e4d4b83e6
jeewon-yoon <elleypsy@gmail.com>
2025-07-24 20:46:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ st.markdown("""
 
 try:
     # 데이터 로드
-    df = load_data('data/sunspots.csv')
+    df = load_data('C:/Users/elley/Desktop/메타코드/data/sunspots.csv')
 
     # 필터링된 데이터 - 전체 데이터 사용
     filtered_df = df
